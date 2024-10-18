aa32b4c54af09fd9dc979fd181b4359a618ed681
dolphin <veedumb@gmail.com>
2024-10-18 12:45:40 +0300

    do a little bit of refactoring in the seed.go

--- scripts/seed.go
+++ scripts/seed.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/VeeRomanoff/hotel-reservation/db"
 	"github.com/VeeRomanoff/hotel-reservation/types"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -11,22 +10,32 @@ import (
 	"log"
 )
 
-func main() {
-	ctx := context.Background()
+var (
+	client     *mongo.Client
+	roomStore  db.RoomStore
+	hotelStore db.HotelStore
+	ctx        = context.Background()
+)
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+func init() {
+	var err error
+	client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	hotelStore := db.NewMongoHotelStore(client)
-	roomStore := db.NewMongoRoomStore(client, hotelStore)
+	if err = client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal(err)
+	}
+	hotelStore = db.NewMongoHotelStore(client)
+	roomStore = db.NewMongoRoomStore(client, hotelStore)
+}
 
+func seedHotel(name, location string) {
 	hotel := types.Hotel{
-		Name:     "Five seasons",
-		Location: "France",
+		Name:     name,
+		Location: location,
 		Rooms:    []primitive.ObjectID{},
 	}
-
 	rooms := []types.Room{
 		{
 			Type:      types.SingleRoomType,
@@ -45,19 +54,21 @@ func main() {
 			BasePrice: 799.9,
 		},
 	}
-	fmt.Println("seeding the database...")
 	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("inserted hotel:", insertedHotel)
-
 	for _, room := range rooms {
 		room.HotelID = insertedHotel.ID
-		insertedRoom, err := roomStore.InsertRoom(ctx, &room)
+		_, err := roomStore.InsertRoom(ctx, &room)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("inserted room: ", insertedRoom)
 	}
+}
+
+func main() {
+	seedHotel("Bellucia", "France")
+	seedHotel("Five seasons", "The Netherlands")
+	seedHotel("Sultan Ahmet", "Turkey")
 }
